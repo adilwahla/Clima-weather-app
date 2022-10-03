ac1365987e06c71c376f86f2a4debcd38a6ab8b5
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-10-03 15:24:58 +0530

    Added app

---
+++ clima_weather_app/lib/screens/loading_screen.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../services/weather.dart';
+import 'location_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    getLocationData();
+  }
+
+  void getLocationData() async{
+  var weatherData= await WeatherModel().getLocationWeather();
+
+Navigator.push(context,MaterialPageRoute(builder: (context){
+  return LocationScreen(locationWeather:weatherData);
+}));
+
+
+  }
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+         child: SpinKitDoubleBounce(
+           color: Colors.white,
+           size: 100.0,
+         ),
+      ),
+    );
+  }
+}
